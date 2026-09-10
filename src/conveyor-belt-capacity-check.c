535b10648034a3754f0363935daaa608ed9940b0
Alexander Ibsen <din-email@her.dk>
2026-09-10 17:19:48 +0200

    Made inputs possible with integer values, and used an operator comparison function to check if the total MOTOR_CAPACITY is high enough, so that the conveyor belt can move

--- src/conveyor-belt-capacity-check.c
+++ src/conveyor-belt-capacity-check.c
@@ -5,5 +5,27 @@ int MOTOR_CAPACITY = 12;
 int main() {
     int motorCount, totalPackageWeight;
 
-    /// YOUR CODE HERE
-}
+    /// References:
+    // https://www.geeksforgeeks.org/c/basic-input-and-output-in-c/
+    // https://www.w3schools.com/c/c_operators_comparison.php
+    //
+
+    // Define integer values
+    printf("How many motors are carrying the packages?\n");
+  
+    scanf("%d", &motorCount);
+    
+    printf("%d\n", motorCount);
+  
+    printf("How many kg of packages do we expect?\n");
+  
+    scanf("%d", &totalPackageWeight);
+    
+   	printf("%d\n", totalPackageWeight);
+
+    // compare totalPackageWeight with total Motor Capacity
+    if (totalPackageWeight <= (motorCount * MOTOR_CAPACITY)) {
+        printf("Yes! The conveyor belt can carry the packages.");
+    } else {
+        printf("No. The conveyor belt cannot carry the packages.");
+    }
